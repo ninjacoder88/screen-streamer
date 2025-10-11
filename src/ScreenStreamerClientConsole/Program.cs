1f0043a59aec714e5e3ed3b39e1f4e542bbc5500
ninjacoder88 <tom@ninjasoft.io>
2025-10-11 14:17:49 -0500

    Added console app for testing recevie from client

---
+++ src/ScreenStreamerClientConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Net.Sockets;
+
+Console.WriteLine("Starting");
+
+IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 42069);
+
+UdpClient udpClient = new UdpClient(42069);
+Console.WriteLine("Listening");
+
+while(true)
+{
+    try
+    {
+        byte[] array = udpClient.Receive(ref iPEndPoint);
+        Console.WriteLine($"Received {array.Length} bytes");
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
