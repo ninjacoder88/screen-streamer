8048da7f0e479e1854146a842f12202158ab6b25
ninjacoder88 <tom@ninjasoft.io>
2025-10-24 18:34:04 -0500

    added compression utiility

---
+++ src/ScreenStreamer.Server/Compressor.cs
@@ -0,0 +1,37 @@
+﻿using System.IO.Compression;
+
+namespace ScreenStreamer.Server
+{
+    internal static class Compressor
+    {
+        public static byte[] Compress(byte[] source)
+        {
+            using (MemoryStream sourceStream = new MemoryStream(source))
+            {
+                using (MemoryStream compressedStream = new MemoryStream())
+                {
+                    using (GZipStream compressionStream = new GZipStream(compressedStream, CompressionLevel.Fastest))
+                    {
+                        sourceStream.CopyTo(compressionStream);
+                        return compressedStream.ToArray();
+                    }
+                }
+            }
+        }
+
+        public static byte[] Decompress(byte[] source)
+        {
+            using(MemoryStream sourceStream = new MemoryStream(source))
+            {
+                using(MemoryStream decompressedStream = new MemoryStream())
+                {
+                    using (GZipStream decompressionStream = new GZipStream(sourceStream, CompressionMode.Decompress))
+                    {
+                        decompressedStream.CopyTo(decompressedStream);
+                        return decompressedStream.ToArray();
+                    }
+                }
+            }
+        }
+    }
+}
